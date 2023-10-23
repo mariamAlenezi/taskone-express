ccab20620cb1eeeda168d7cd3eaae3751a892623
mariamAlenezi <144369907+mariamAlenezi@users.noreply.github.com>
2023-10-23 16:57:53 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ const products = require("./Data");
 
 const app = express();
 
-app.post("/api/products/:id", (req, res) => {
-  res.status(200).json(products);
-});
 
 app.listen(8000, () => {
   console.log("The application is running on localhost:8000");
